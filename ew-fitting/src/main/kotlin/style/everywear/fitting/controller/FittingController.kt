b755266a143825ff642ee03476324b1a1b4a769c
jun097kim <jun097kim@gmail.com>
2019-05-20 05:01:37 +0900

    Add uploading profile image to file system

--- ew-fitting/src/main/kotlin/style/everywear/fitting/controller/FittingController.kt
+++ ew-fitting/src/main/kotlin/style/everywear/fitting/controller/FittingController.kt
@@ -1,10 +1,41 @@
 package style.everywear.fitting.controller
 
-import org.springframework.web.bind.annotation.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import style.everywear.fitting.service.FileSystemUploadService
+import java.util.*
 
 @RestController
 @RequestMapping("/v1/fitting")
 class FittingController {
 
+    @Autowired
+    val fileSystemUploadService = FileSystemUploadService()
+
+    @RequestMapping(value = ["/"], method = [RequestMethod.POST], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun requestFitting(@RequestParam("profile") file: MultipartFile): ResponseEntity<Map<String, String>> {
+
+        var entity: ResponseEntity<Map<String, String>>
+        var response: Map<String, String>
+
+        try {
+            fileSystemUploadService.upload(file)
+            response = Collections.singletonMap("message", "사진을 성공적으로 업로드하였습니다.")
+            entity = ResponseEntity(response, HttpStatus.OK)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            response = Collections.singletonMap("message", "사진 업로드에 실패하였습니다.")
+            entity = ResponseEntity(response, HttpStatus.BAD_REQUEST)
+        }
+
+        return entity
+    }
 }
---
+++ ew-fitting/src/main/kotlin/style/everywear/fitting/service/FileSystemUploadService.kt
@@ -0,0 +1,26 @@
+package style.everywear.fitting.service
+
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.sql.Timestamp
+
+
+@Service
+class FileSystemUploadService {
+    companion object {
+        const val UPLOAD_PATH = "/Users/jun097kim/dev/everywear-model-server/uploads/"
+    }
+
+    fun upload(file: MultipartFile) {
+        val bytes: ByteArray = file.bytes
+
+        val timestamp = Timestamp(System.currentTimeMillis())
+        val filename = "${timestamp.time}_${file.originalFilename}"
+        val path = Paths.get(UPLOAD_PATH + filename)
+
+        Files.createDirectories(path.parent)
+        Files.write(path, bytes)
+    }
+}
