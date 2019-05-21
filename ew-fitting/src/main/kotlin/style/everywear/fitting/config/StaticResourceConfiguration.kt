fc30708296e3983ac77d999a89d9a0f36b31c6d8
jun097kim <jun097kim@gmail.com>
2019-05-21 11:25:36 +0900

    Add serving static files

---
+++ ew-fitting/src/main/kotlin/style/everywear/fitting/config/StaticResourceConfiguration.kt
@@ -0,0 +1,16 @@
+package style.everywear.fitting.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class StaticResourceConfiguration: WebMvcConfigurer {
+
+    // Serving static files
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+                .addResourceHandler("/uploads/**")
+                .addResourceLocations("file:/Users/jun097kim/dev/everywear-model-server/uploads/")
+    }
+}
